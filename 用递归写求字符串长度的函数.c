cfeec7d31cccd2a02ecf7d9daee5fdc89e218312
陈镝宇 <101394847+chendiyu1113@users.noreply.github.com>
2022-03-19 17:29:02 +0800

    Add files via upload

---
+++ 用递归写求字符串长度的函数.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int my_strlen(char* str)
+{
+	if (*str != '\0')
+	{
+		return 1 + my_strlen(str + 1);
+	}
+	else
+		return 0;
+
+}
+
+
+int main()
+{
+	char arr []= "chen";
+	printf("%d\n", my_strlen(arr));
+
+	return 0;
+}
